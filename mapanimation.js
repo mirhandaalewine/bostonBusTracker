cca1075813099a9dd99ee717df958de7dba72b4f
Mirhanda <96195059+mirhandaalewine@users.noreply.github.com>
2022-03-16 08:55:10 -0400

    Add files via upload

---
+++ mapanimation.js
@@ -0,0 +1,38 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibWlyaGFuZGFhbGV3aW5lIiwiYSI6ImNsMG1vMHhlcDB3bzgza28zeDFuY3h2MjcifQ.1wVfPTJA841rtUA4gd1yww';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://style/mapbox/streets-v11',
+  center: [-71.0942, 42.3601],
+  zoom: 14
+});
+
+const busMarkers = {};
+
+async function run(){
+    // get bus data    
+	const locations = await getBusLocations();
+	console.log(new Date());
+	console.log(locations);
+
+	locations.forEach((bus) => {
+		if (busMarkers[bus.id]) {
+			busMarkers[bus.id].setLngLat([bus.attributes.longitude, bus.attributes.latitude]);
+		} else {
+			busMarkers[bus.id] = new mapboxgl.Marker().setLngLat([bus.attributes.longitude, bus.attributes.latitude]).addTo(map);
+		}
+	})
+
+	// timer
+	setTimeout(run, 15000);
+}
+
+// Request bus data from MBTA
+async function getBusLocations(){
+	const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
+	const response = await fetch(url);
+	const json     = await response.json();
+	return json.data;
+}
+
+run();
